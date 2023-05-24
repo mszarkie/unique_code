190908dfa2d81b86599b1bf46fca8d7d72bbd36f
Michal <michal.szarek48@gmail.com>
2023-05-24 14:57:09 +0200

    here i made code more likley for eye

--- alpha.py
+++ alpha.py
@@ -1,5 +1,16 @@
 from random import randint, choice
 
+
 def uno(surname, name):
     alphabet = 'qwertyuiopasdfghjklzxcvbnm'
     return choice(alphabet) + str(randint(1, 9)) + surname[0] + name[1] + '-'
+
+
+def dos(surname, name):
+    a = len(name)
+    b = len(surname)
+    return choice(surname) + str(a*b) + '-'
+
+
+def tres(surname):
+    return surname[2::-2] + str(randint(10, 99))
--- testinggg.py
+++ testinggg.py
@@ -1,18 +1,9 @@
-from random import randint, choice
+from random import choice
 from datetime import datetime
-from alpha import uno
-
-actual_date = datetime.now()
-seconds = actual_date.strftime('%S')
-micro_seconds = actual_date.strftime('%f')
-
-
-# print(seconds[-1])
-#
-# print(micro_seconds[0::4])
-
+from alpha import uno, dos, tres
 
 name = input("Enter your name: ")
+surname = input("Enter your surname: ")
 
 """while True:
     if len(name) in range(10):
@@ -24,11 +15,6 @@ name = input("Enter your name: ")
     else:
         print("Wrong syntax. Enter your name again")"""
 
-surname = input("Enter your surname: ")
-
-
-
-
 """while True:
     if len(name) in range(10):
         pass
@@ -39,8 +25,15 @@ surname = input("Enter your surname: ")
     else:
         print("Wrong syntax. Enter name again")"""
 
-a = len(name)
-b = len(surname)
+actual_date = datetime.now()
+seconds = actual_date.strftime('%S')
+micro_seconds = actual_date.strftime('%f')
+
+
+# print(seconds[-1])
+#
+# print(micro_seconds[0::4])
+
 uniqueCodeList = []
 
 alphabet = 'qwertyuiopasdfghjklzxcvbnm'
@@ -48,19 +41,11 @@ alphabet = 'qwertyuiopasdfghjklzxcvbnm'
 two_letters = choice(alphabet) + choice(alphabet)
 print("this is choice x2:", two_letters)
 
-<<<<<<< HEAD# alpha = choice(alphabet) + str(randint(1, 9)) + surname[0] + name[1] + '-'
-beta = choice(surname) + str(a*b) + '-'
-omega = surname[2::-2] + str(randint(10, 99))
+a = uno(surname, name)
+b = dos(surname, name)
+c = tres(surname)
 
-
-unique_code = uno(surname, name) + beta + omega
-
-alpha = choice(alphabet) + str(randint(1, 9)) + surname[0] + name[1] + '-'
-beta = choice(surname) + str(a*b) + '-'
-omega = surname[2::-2] + str(randint(10, 99))
-
-unique_code = alpha + beta + omega
-
+unique_code = a + b + c
 
 uniqueCodeList.append(unique_code.upper())
 
